6a8a711727b220f906aff67f4844a766ef50b5cc
Davit Karchikyan <davit.karchikyan@iunetworks.am>
2019-05-31 10:47:07 +0400

    create UserDao interface

---
+++ src/main/java/am/xntrens/dao/UserDao.java
@@ -0,0 +1,14 @@
+package am.xntrens.dao;
+
+import am.xntrens.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author David Karchikyan.
+ * This {@link UserDao} interface presents abstract actions for {@link UserEntity}
+ */
+
+@Repository
+public interface UserDao extends JpaRepository<UserEntity, Integer> {
+}
